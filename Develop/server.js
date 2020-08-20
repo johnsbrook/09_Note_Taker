5932459b7e0e1379f8b4ba4b38bfce08ac1c835e
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-20 18:29:49 -0400

    GET * will return index.html

--- Develop/server.js
+++ Develop/server.js
@@ -15,3 +15,8 @@ app.use(express.json());
 app.get('/notes', function(req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
 });
+
+// GET * - Should return the index.html file
+app.get('*', function(req,res){
+    res.sendFile(path.join(__dirname, "index.html"));
+})
