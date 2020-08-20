01ab67442843a79f6df8879345d04dbca30ae433
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-20 18:41:02 -0400

    added notes for next steps

--- Develop/server.js
+++ Develop/server.js
@@ -26,4 +26,6 @@ app.get('*', function(req,res){
 // GET /api/notes - Should read the db.json file and return all saved notes as JSON
 app.get('/api/notes', function(req, res){
     return res.json(notes);
-})
+});
+
+// POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
