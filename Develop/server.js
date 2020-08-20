142b4655d4bf16c4c38543874c0b50512272b2c8
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-20 18:18:50 -0400

    set up express app to handle data parsin

--- Develop/server.js
+++ Develop/server.js
@@ -7,5 +7,7 @@ var app = express();
 // Set initial port
 var PORT = process.env.PORT || 8080;
 
-
+// Set up Express app to handle data parsing
+app.use(express.urlencoded({extend: true}));
+app.use(express.json());
 
