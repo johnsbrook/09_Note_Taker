6d6fdf4253bf3d94be258dcff4e0cef04991ef6e
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-20 20:43:32 -0400

    added gitignore

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -35,11 +35,9 @@ app.get('/api/notes', function(req, res){
 app.post('api/notes', function(req, res) {
 
     var newNote = req.body;
-
         newNote.title = newNote.title.replace(/\s+/g, '').toLowerCase();
     console.log(newNote);
     notes.push(newNote);
-
 })
 
 // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
