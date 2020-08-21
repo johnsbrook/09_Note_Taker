cbb1ecb49d3057e61340cad9c286c312062d1f0a
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-20 21:21:06 -0400

    established API routing

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,7 @@
 // Linked db.json to routing
 var notesData = require("../db/db.json");
 
-// Established routing
+// Established API routing
 module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
