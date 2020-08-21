f8069f7ffcb6b58bae736b6fbe2288829abc98d4
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-20 20:06:41 -0400

    added express installed files and folders

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -16,12 +16,12 @@ app.use(express.json());
 
 // GET /notes - Should return the notes.html file.
 app.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "notes.html"));
 });
 
 // GET * - Should return the index.html file
 app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 })
 
 // API routes should be created
