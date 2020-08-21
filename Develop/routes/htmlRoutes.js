4f8549d663cfb610178c95654482e9a5f64ac1b7
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-20 21:29:39 -0400

    updated htmlroutes.js

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -4,7 +4,6 @@ var path = require("path");
 // HTML routing
 module.exports = function(app) {
 
-
     app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
