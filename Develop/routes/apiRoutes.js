3dae3e3d38a8d01a0c7c1aac96bc7d6b33ecd62e
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-20 21:20:24 -0400

    established routing

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,7 +3,19 @@ var notesData = require("../db/db.json");
 
 // Established routing
 module.exports = function(app) {
+
     app.get("/api/notes", function(req, res) {
         res.json(notesData);
-      });
-}
+    });
+
+    app.post("/api/notes", function(req, res) {
+        notesData.push(req.body);
+        res.josn(true);
+    });
+
+    app.post("/api/notes", function(req, res) {
+        notesData.length = 0;
+        res.json({ok: true});
+    });
+
+};
