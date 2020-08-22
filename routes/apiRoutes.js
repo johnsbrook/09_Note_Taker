065e4f874787d5b461020c028761c60544633c88
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-21 23:15:54 -0400

    added api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,7 @@
-// Linked db.json to routing
-var notesData = require("../db/db.json");
+var baseDatos = require('../db/db.json');
 
-// Established API routing
-module.exports = function(app) {
-
-    app.get("/api/notes", function(req, res) {
-        return res.json(notesData);
-    });
-
-    app.post("/api/notes", function(req, res) {
-        notesData.push(req.body);
-        return res.json();
-    });
-
-};
-
+module.exports = function (app) {
+    app.get('/api/notes', function (sol, res){
+        res.baseDatos;
+    })
+}
