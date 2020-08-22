e74b91b7d33aff77a0d6736ad4e21d1927a256cc
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-22 00:27:19 -0400

    replaced api html routings

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,22 @@
-var baseDatos = require('../db/db.json');
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+var notesData = require('../db/db.json');
 
-module.exports = function (app) {
-    app.get('/api/notes', function (sol, res){
-        res.baseDatos;
-    })
-}
+module.exports = function(app) {
+
+  app.get("/api/notes", function(req, res) {
+    res.json(notesData);
+  });
+
+  app.post("/api/notes", function(req, res) {
+    notesData.push(req.body);
+    res.json(true);  
+  });
+
+  app.post("/api/clear", function(req, res) {
+    notesData.length = 0;
+    res.json({ ok: true });
+  });
+};
--- server.js
+++ server.js
@@ -1,13 +1,39 @@
-var express = require('express');
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({extended: true}));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/apiRoutes');
-require('./routes/htmlRoutes');
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 
 app.listen(PORT, function() {
-    console.log("Esta aplicacion esta usando el puerto " + PORT + " y esta disponible en http://localhost:" + PORT);
-})
+  console.log("App listening on PORT: " + PORT);
+});
